e7da1a4879c0e434fa6276f44620ef48f61d2f0e
Amara Chiaha <catherinechiaha@gmail.com>
2021-02-24 14:36:11 +0100

    Create footer.vue

---
+++ src/components/footer.vue
@@ -0,0 +1,35 @@
+<template>
+    <footer>
+      <p>{{ft}} </p>
+    </footer>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      ft:"Designed with love for money"
+    }
+  
+}
+
+ }
+</script>
+
+<style scoped>
+
+  footer{
+    /* margin-top: 300px  !important; */
+    background: #ff4646;
+    padding: 6px;
+  }
+
+  p{
+    text-align: center;
+    color: #ffffff;
+  }
+
+  
+
+</style>
