f8295340695f5ff747c1e7937b4940b8c1df4bd2
Amara Chiaha <catherinechiaha@gmail.com>
2021-02-24 14:36:37 +0100

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <nav-item></nav-item>
+    <home-page></home-page>
+    <footer-item></footer-item>
+    
+  </div>
+</template>
+
+<script>
+import nav from './components/nav.vue'
+import footer from './components/footer.vue'
+import home from './components/home.vue'
+ export default{
+   components:{
+     'nav-item': nav,
+     'footer-item': footer,
+     'home-page': home
+   },
+  data(){
+    return{
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
